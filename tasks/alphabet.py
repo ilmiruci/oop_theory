8006b42e38aa0a273a679a72659da2c45f789a62
ilmir <adm.eths@gmail.com>
2025-05-19 21:02:11 +0500

    Правки по комментариям

--- tasks/alphabet.py
+++ tasks/alphabet.py
@@ -12,15 +12,13 @@ class Alphabet:
         return len(self.letters)
 
 class EngAlphabet(Alphabet):
+    __letters_num: int = 26
+
     def __init__(self) -> None:
         super().__init__(language="En", letters=string.ascii_uppercase)
-        self.__letters_num = len(self.letters)
 
-    def is_in_letter(self, letter: str):
-        if letter.upper() in self.letters:
-            print(f"Буква {letter} есть в алфавите")
-        else:
-            print(f"Буквы {letter} нет в алфавите")
+    def is_in_letter(self, letter: str) -> bool:
+        return letter.upper() in self.letters
 
     def letters_num(self) -> int:
         return self.__letters_num
@@ -38,6 +36,6 @@ if __name__ == "__main__":
     en = EngAlphabet()
     en.print()
     print(en.letters_num())
-    en.is_in_letter("F")
-    en.is_in_letter("Щ")
+    print(en.is_in_letter("F"))
+    print(en.is_in_letter("Щ"))
     print(en.example())
